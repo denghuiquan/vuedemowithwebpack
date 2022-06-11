ec50903b229c0ffd1e2c9ffda1a2a55b7c071853
denghuiquan <980352129@qq.com>
2022-06-11 16:30:54 +0800

    add vue develop package as a devDependencie

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div class="content">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>{{ titile }}
+      <button @click="count++"> Count is: {{ count }}</button>
+    </p>
+    <HelloWorld :msg="welcomeTips" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: "App",
   components: {
     HelloWorld,
   },
+  data() {
+    return {
+      titile: 'Welcome to Vue Development.',
+      count: 0
+    }
+  },
+  computed: {
+    welcomeTips() {
+      return `Welcome to Your Vue.js App ${this.count}`
+    }
+  }
 };
 </script>
 
-<style>
-#app,.content {
+<style lang="less" scopeds>
+#app,
+.content {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+button {
+  font-weight: bold;
 }
 </style>
